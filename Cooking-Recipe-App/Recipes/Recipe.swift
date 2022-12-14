31fe5b00f42e25e48479c54aa2f7a1af03d99bcc
Nofal-Helal <202001264@student.polytechnic.bh>
2022-12-14 11:43:27 +0300

    Add Recipe list view Also add recipe detail view in another storyboard

--- Cooking-Recipe-App/Recipes/Recipe.swift
+++ Cooking-Recipe-App/Recipes/Recipe.swift
@@ -25,3 +25,28 @@ struct Ingredient: Codable {
     var measurement: Measurement<Unit>
     var text: String
 }
+
+
+extension Recipe {
+    static let sample_recipes: [Recipe] = [
+        Recipe(
+            title: "Zeppole di San Giuseppe",
+            categories: ["Italian", "Dessert"],
+            source: "Cristene",
+            yield: "12 pieces",
+            preparationTime: TimeInterval(60*30),
+            cookingTime: TimeInterval(60*35),
+            description: "Traditional Italian Dessert",
+            ingredients: [
+                Ingredient(measurement: Measurement(value: 4, unit: UnitVolume.fluidOunces), text: "butter"),
+                Ingredient(measurement: Measurement(value: 1.5, unit: UnitVolume.teaspoons), text: "sugar"),
+                Ingredient(measurement: Measurement(value: 1.5, unit: UnitVolume.teaspoons), text: "flour"),
+            ],
+            directions: [
+                "Heat oven to 400ºF",
+                "First prepare the choux pastry",
+                "Remove from heat",
+            ]
+        )
+    ]
+}
---
+++ Cooking-Recipe-App/Recipes/RecipeTableViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  RecipeTableViewCell.swift
+//  Cooking-Recipe-App
+//
+//  Created by Student on 14/12/2022.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet var recipeImageView: UIImageView!
+    
+    func configure(forRecipe recipe: Recipe) {
+        
+        titleLabel.text = recipe.title
+        descriptionLabel.text = recipe.description
+        
+        if let image = UIImage(named: "zeppole") {
+            recipeImageView.image = image
+        }
+        
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
