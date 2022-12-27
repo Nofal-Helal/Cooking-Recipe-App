0d1bc13d9b62c504ad4ecbd5cfe909d56247ac67
SM3176 <202003176@student.polytechnic.bh>
2022-12-27 09:15:50 +0300

    finished settings but it do no work

---
+++ Cooking-Recipe-App/Settings/HeaderTitleCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderTitleCell.swift
+//  Cooking-Recipe-App
+//
+//  Created by Student on 27/12/2022.
+//
+
+import UIKit
+
+class HeaderTitleCell: UITableViewCell {
+
+    @IBOutlet var titles: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Cooking-Recipe-App/Settings/SettingsViewController.swift
+++ Cooking-Recipe-App/Settings/SettingsViewController.swift
@@ -9,21 +9,83 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    var headers = ["Settings","Stay toned"]
+    var sets = ["Measurement System","Display Theme","Rate US"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        registerTableCells()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func registerTableCells(){
+        tableView.register(UINib(nibName: "HeaderTitleCell", bundle: nil), forCellReuseIdentifier: "HeaderTitleCell")
     }
-    */
 
+}
+
+extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return headers.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            return sets.count
+        default:
+            return 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HeaderTitleCell") as! HeaderTitleCell
+        cell.titles.font = UIFont.systemFont(ofSize: 15)
+        cell.backgroundColor = .clear
+        cell.contentView.backgroundColor = .clear
+        
+        switch indexPath.section {
+        case 0:
+            cell.titles.text = sets[indexPath.row]
+        default:
+            cell.titles.text = sets[indexPath.row]
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "HeaderTitleCell") as! HeaderTitleCell
+        cell.titles.font = UIFont.boldSystemFont(ofSize: 20)
+        cell.backgroundColor = .white
+        cell.contentView.backgroundColor = .white
+        cell.titles.text = headers[section]
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 60
+        default:
+            return 60
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 40
+        default:
+            return 40
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 20
+        default:
+            return 20
+        }
+    }
 }
