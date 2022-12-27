c42374e650c1ffb046e86f930ce73fcb43cb54d3
SM3176 <202003176@student.polytechnic.bh>
2022-12-27 11:31:50 +0300

    added init to details not finished

--- Cooking-Recipe-App/RecipeDetailViewController.swift
+++ Cooking-Recipe-App/RecipeDetailViewController.swift
@@ -8,22 +8,24 @@
 import UIKit
 
 class RecipeDetailViewController: UIViewController {
-
+    
+    var recipe: Recipe!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    required init?(coder: NSCoder, recipe: Recipe) {
+        super.init(coder: coder)
+        self.recipe = recipe
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 
 }
