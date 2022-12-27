b4e0e0d7448dcca9d9f0afa2c39805886c4f6045
Nofal-Helal <202001264@student.polytechnic.bh>
2022-12-27 13:19:32 +0300

    [Recipe Add/Edit] Image picker

--- Cooking-Recipe-App/Recipes/RecipeEditor/OverviewTableViewController.swift
+++ Cooking-Recipe-App/Recipes/RecipeEditor/OverviewTableViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class OverviewTableViewController: UITableViewController {
+class OverviewTableViewController: UITableViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet var recipeTitleField: UITextField!
     @IBOutlet var sourceField: UITextField!
@@ -17,9 +17,14 @@ class OverviewTableViewController: UITableViewController {
     @IBOutlet var cookingTimePicker: UIDatePicker!
     @IBOutlet var descriptionTextView: UITextView!
     
+    @IBOutlet var recipeImageView: UIImageView!
+    @IBOutlet var cameraIcon: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        recipeImageView.layer.cornerRadius = recipeImageView.frame.width/2
+        cameraIcon.layer.cornerRadius = cameraIcon.frame.width/4
     }
 
     @IBAction func stepperPressed(_ sender: UIStepper) {
@@ -30,5 +35,47 @@ class OverviewTableViewController: UITableViewController {
         yieldStepper.value = (try? Double(yieldField.text ?? "0", format: .number)) ?? yieldStepper.value
     }
     
+    
+    @IBAction func recipeImagePressed(_ sender: UITapGestureRecognizer) {
+        if sender.state == .ended {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            
+            let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+            
+            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+                alertController.addAction(UIAlertAction(title: "Choose Photo", style: .default) { _ in
+                    imagePicker.sourceType = .photoLibrary
+                    self.present(imagePicker, animated: true, completion: nil)
+                })
+            }
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                alertController.addAction(UIAlertAction(title: "Take Photo", style: .cancel) { _ in
+                    imagePicker.sourceType = .camera
+                })
+            }
+            
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+            
+            alertController.popoverPresentationController?.sourceView = sender.view
+            present(alertController, animated: true)
+
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.originalImage] as? UIImage
+        if let imageData = image?.jpegData(compressionQuality: 0.9),
+           let image = UIImage(data: imageData) {
+            recipeImageView.image = image
+            picker.dismiss(animated: true)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true)
+    }
+
     
 }
