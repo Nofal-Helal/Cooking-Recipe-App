7de87461965127b81add8c89736d09e0b5cee515
SM3176 <202003176@student.polytechnic.bh>
2022-12-28 10:50:49 +0300

    finished dark and semi details

--- Cooking-Recipe-App/RecipeDetailViewController.swift
+++ Cooking-Recipe-App/RecipeDetailViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 class RecipeDetailViewController: UIViewController {
     
     var recipe: Recipe!
+    @IBOutlet var dTitle: UILabel!
+    @IBOutlet var dDesc: UILabel!
+    @IBOutlet var categories: Tags!
+    
+    @IBOutlet var prepTime: UILabel!
+    @IBOutlet var cookTime: UILabel!
+    @IBOutlet var yield: UILabel!
+    @IBOutlet var ingredTableView: UITableView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        loadStuff()
     }
     
     required init?(coder: NSCoder, recipe: Recipe) {
@@ -27,5 +36,64 @@ class RecipeDetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-
+    func  loadStuff(){
+        
+        
+        dTitle.text = recipe.title
+        dDesc.text = recipe.description
+        //categories = recipe.categories
+        prepTime.text = String(recipe.preparationTime)
+        cookTime.text = String(recipe.cookingTime)
+        yield.text = String(recipe.yield)
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return recipe.ingredients.count
+    }
+    
+   /* func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Ingreds")
+        
+        cell.ingrs.text = recipe.ingredients[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Ingreds")
+        cell.font = UIFont.boldSystemFont(ofSize: 20)
+        cell.text = "Ingredients"
+        return cell
+    }*/
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 10
+        default:
+            return 10
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 20
+        default:
+            return 20
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        switch section {
+        case 0:
+            return 10
+        default:
+            return 10
+        }
+    }
+    
 }
--- Cooking-Recipe-App/Settings/SettingsViewController.swift
+++ Cooking-Recipe-App/Settings/SettingsViewController.swift
@@ -8,29 +8,49 @@
 import UIKit
 import Foundation
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var userName: UILabel!
+    @IBOutlet var textF: UITextField!
     var headers = ["Settings","Stay toned"]
     var sets = ["Measurement System","Display Theme","Rate US"]
+    var n: [Int] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         registerTableCells()
+        
     }
     
     func registerTableCells(){
         tableView.register(UINib(nibName: "HeaderTitleCell", bundle: nil), forCellReuseIdentifier: "HeaderTitleCell")
     }
+    
+    func ImageTapped() {
+        userName.isHidden = true
+                textF.isHidden = false
+                textF.text = userName.text
+    }
+    func textFieldShouldReturn(userText: UITextField) -> Bool {
+            userText.resignFirstResponder()
+            textF.isHidden = true
+            userName.isHidden = false
+            userName.text = textF.text
+            return true
+        }
+    
+    
 
 }
 
 extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
+    //how many headers
     func numberOfSections(in tableView: UITableView) -> Int {
         return headers.count
     }
-    
+    //return number how rows
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
         case 0:
@@ -40,6 +60,7 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
         }
     }
     
+    //prints rows and change design
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HeaderTitleCell") as! HeaderTitleCell
         cell.titles.font = UIFont.systemFont(ofSize: 15)
@@ -55,13 +76,14 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
 
         return cell
     }
-    
+    //prints header and change design
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let cell = tableView.dequeueReusableCell(withIdentifier: "HeaderTitleCell") as! HeaderTitleCell
         cell.titles.font = UIFont.boldSystemFont(ofSize: 20)
         cell.titles.text = headers[section]
         return cell
     }
+    //design stuff
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         switch indexPath.section {
         case 0:
@@ -70,7 +92,7 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
             return 60
         }
     }
-    
+    //this too
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         switch section {
         case 0:
@@ -79,7 +101,7 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
             return 40
         }
     }
-    
+    //also this
     func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
         switch section {
         case 0:
@@ -88,19 +110,21 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
             return 20
         }
     }
-    
+    //when a row is pressed
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.row {
+            //for measurement system row
         case 0:
             let setTitle = sets[indexPath.row]
             
             let alertController = UIAlertController(title: "Measurement System", message: setTitle, preferredStyle: .actionSheet)
-            alertController.addAction(UIAlertAction(title: "US Customary Units", style: .default) { _ in })
-            alertController.addAction(UIAlertAction(title: "Metric", style: .default) { _ in })
-            alertController.addAction(UIAlertAction(title: "UK Imperial", style: .destructive) { _ in })
+            alertController.addAction(UIAlertAction(title: "US Customary Units", style: .default) { _ in self.measurementSys(num: 1)})
+            alertController.addAction(UIAlertAction(title: "Metric", style: .default) { _ in self.measurementSys(num: 2)})
+            alertController.addAction(UIAlertAction(title: "UK Imperial", style: .destructive) { _ in self.measurementSys(num: 3)})
             alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
             
             self.present(alertController, animated: true)
+            //for display them row
         case 1:
             let setTitle = sets[indexPath.row]
             
@@ -110,6 +134,7 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
             alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel))
             
             self.present(alertController, animated: true)
+            //just extra option (look good) :)
         case 2:
             let setTitle = sets[indexPath.row]
             
@@ -123,18 +148,52 @@ extension SettingsViewController: UITableViewDelegate,UITableViewDataSource{
             ()
         }
     }
-    
+    //funtion to change between dark and light modes
     func DisplayTheme(num: Int){
-        
-        if(num == 1){
-            overrideUserInterfaceStyle = .light
-
-        }else if(num == 2){
-            overrideUserInterfaceStyle = .dark
-            
+        if #available(iOS 13.0, *){
+            let appDelegate = UIApplication.shared.windows.first
+            if(num == 1){
+                appDelegate?.overrideUserInterfaceStyle = .light
+                n[0] = 1
+                return
+            }else if(num == 2){
+                appDelegate?.overrideUserInterfaceStyle = .dark
+                n[0] = 2
+                return
+            }
         }
-            
-        
     }
+    // for measurement system
+    func measurementSys(num: Int){
+        if(num == 1){
+            n[1] = 1
+        }else if (num == 2){
+            n[1] = 2
+        }else if (num == 3){
+            n[1] = 3
+        }
+    }
+    //creating fileManager
+    static var archiveURL: URL {
+        var documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+            documentsURL.appendPathComponent("Settings.plist")
+        return documentsURL
+    }
+    //saving username, theme and measure
+    static func saveRecipes(_ n: Int) {
+        let plistEncoder = PropertyListEncoder()
+        if let encodedRecipes = try? plistEncoder.encode(n) {
+            try? encodedRecipes.write(to: archiveURL)
+        }
+    }
+    //load the stuff
+    /*static func loadRecipes() -> [Recipe]? {
+        let plistDecoder = PropertyListDecoder()
+        if let encodedRecipes = try? Data(contentsOf: archiveURL),
+           let decodedRecipes = try? plistDecoder.decode([Recipe].self, from: encodedRecipes) {
+            return decodedRecipes
+        }
+        return nil
+    }*/
     
 }
